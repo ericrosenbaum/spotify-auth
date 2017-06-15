619743d4dae4112bfd9fec465e608dd854ec804a
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-06-15 17:13:51 -0400

    Return json formatted token

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -1,18 +1,14 @@
 /**
- * This is an example of a basic node.js script that performs
- * the Client Credentials oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
+ * Use the Spotify Client Credentials oAuth2 flow to obtain an access token
  * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
  */
 
-var request = require('request'); // "Request" library
+var request = require('request'); 
+var http = require('http');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
 
-// your application requests authorization
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
@@ -24,20 +20,33 @@ var authOptions = {
   json: true
 };
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
+function getToken(){
+  return new Promise((resolve, reject) => {
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(body.access_token);
+      } else {
+        reject();
+      }
     });
-  }
-});
+  });
+}
+
+exports.handler = (event, context, callback) => {
+  console.log('handler');
+  getToken().then((token) => {
+    tokenJson = '{\"token\": \"' + token + '\"}';
+    console.log('token: ' + tokenJson);
+    callback(null, {
+    "statusCode": 200,
+    "headers": {
+        "X-Requested-With": "*",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST,GET,OPTIONS"
+    },
+    "body": tokenJson
+    });
+  });
+};
+
